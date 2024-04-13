cb128776e9f74d93e17095652ed48b26896283d9
Your Name <you@example.com>
2024-04-12 21:37:13 -0300

    Adicionado endpoint /integrantes para retornar lista de integrantes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const path = require('path'); // Módulo para lidar com caminhos de arquivos
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000; // Porta em que o servidor irá ouvir
+const PORT = process.env.PORT || 3000;
 
 // Define o diretório de arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,6 +10,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Endpoint para servir a página inicial
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Dados dos integrantes do grupo
+const data = {
+  'integrantes': [
+    { 'nome': 'Filipe Polnow de Souza' },
+    { 'nome': 'Jose Berton' },
+    { 'nome': 'Vinicius Guimaraes de Oliveira' }
+  ]
+};
+
+// Endpoint para retornar os integrantes
+app.get('/integrantes', (req, res) => {
+  res.json(data);
 });
 
 // Inicia o servidor
